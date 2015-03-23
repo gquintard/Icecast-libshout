b70878525dcc96672021754163ce8c66d5e23da9
Guillaume Quintard <guillaume.quintard@gmail.com>
2015-03-23 16:47:09 +0100

    send the content-type if there is one

--- src/shout.c
+++ src/shout.c
@@ -1925,6 +1925,8 @@ static int create_icy_request(shout_t *self)
 			break;
 		if (queue_printf(self, "icy-br:%s\n\n", bitrate))
 			break;
+		if (self->mime && queue_printf(self,  "Content-Type: %s\r\n", self->mime)
+			break;
 
 		ret = SHOUTERR_SUCCESS;
 	} while (0);
